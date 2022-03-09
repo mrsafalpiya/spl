7a9e32d329317a13992f8c9a5be2e920396c6162
Safal Piya <mrsafalpiya@gmail.com>
2022-03-09 12:28:00 +0545

    Added a template 'spl_template.h' file
    Now any new library that is to be created, can be done easily by copying
    and renaming the file to the name I want.
    
    Inside the file,
    
    - replace `<+name+>` with the library name in lowercase
    - replace `<+NAME+>` with the library name in uppercase
    - replace `<+info+>` with a short info/intro of the library

---
+++ spl_template.h
@@ -0,0 +1,89 @@
+/*
+ * spl_<+name+>.h - https://github.com/mrsafalpiya/spl
+ * no warranty implied; use at your own risk
+
+ * See end of file for license information.
+*/
+
+/*
+ * ================
+ * | Introduction |
+ * ================
+ *
+ * SPL_<+name+>.H - <+info+>
+ *
+ * Author: Safal Piya
+*/
+
+/*
+ * Take a look at the "Tips for scanning the library" section of the `README.md`
+ * file for some tips and tricks to scan this library header file quickly and
+ * easily
+*/
+
+/*
+ * =========
+ * | Usage |
+ * =========
+ *
+ * Do this:
+ *
+ *         #define SPL_<+NAME+>_IMPLEMENTATION
+ *
+ * before you include this file in *one* C or C++ file to create the
+ * implementation.
+*/
+
+/*
+ * ====================
+ * | Header-file mode |
+ * ====================
+*/
+
+#ifndef SPL_<+NAME+>_H
+#define SPL_<+NAME+>_H
+
+
+#endif /* SPL_<+NAME+>_H */
+
+/*
+ * ===================
+ * | Implementations |
+ * ===================
+*/
+
+#ifdef SPL_<+NAME+>_IMPLEMENTATION
+
+
+#endif /* SPL_<+NAME+>_IMPLEMENTATION */
+
+/*
+ * ===============================================
+ * | License - Public Domain (www.unlicense.org) |
+ * ===============================================
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+*/
